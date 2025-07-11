6a8918b6b3f224394b21ffc25d9b2fefebe3c7be
hxh <3148182735@qq.com>
2025-07-11 14:30:09 +0800

    对局参数常量

---
+++ Game/src/main/java/com/game/Constant/ChessGameConstant.java
@@ -0,0 +1,16 @@
+package com.game.Constant;
+
+public class ChessGameConstant {
+    // 等待落子时间
+    public static final Integer interval = 60000;
+    // 扣分阈值
+    public static final Integer SCORE_LIMIT = 50;
+    // 胜者加分
+    public static final Integer winnerScore = 10;
+    // 败者减分
+    public static final Integer loserScore = 5;
+    // 起始分数
+    public static final Integer startScore = 200;
+    // 签到经验
+    public static final Integer signInExp = 100;
+}
